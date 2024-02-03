8e584ad5910b1a0df086306f2405e245aef1d083
unknown <miguellemes005@gmail.com>
2024-02-02 22:18:59 -0300

    rocketseat ignite class project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Ignite Timer
+
+Projeto desenvolvido na Trilha Ignite - @Rocketseat
+
+## stacks
+
+- Typescript
+- React.js
+- Styled Components
+- ESLint
---
+++ src/Router.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { History } from './pages/History'
+import { DeafultLayout } from './layouts/DefaultLayout'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DeafultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
+    </Routes>
+  )
+}
